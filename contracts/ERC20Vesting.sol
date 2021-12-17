89145fa5557bea8a10a47725f60b4211a1ac030c
Dmitry Kharlanchuk <info@talesofchain.com>
2021-12-17 20:28:31 +0300

    Tale Token implementation

---
+++ contracts/ERC20Vesting.sol
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+/**
+ * @title ERC20 contract with vesting functionality
+ *
+ * This implementation does not allow one address to participate in multiple vests.
+ *
+ */
+abstract contract ERC20Vesting is Ownable, ERC20Burnable {
+    using SafeMath for uint256;
+    struct VestingSchedule{
+        bool initialized;
+        uint256 start;
+        uint256 duration;
+        uint256 period;
+        uint256 amountTotal;
+        uint256 released;
+    }
+
+    mapping(address => VestingSchedule) private vestingSchedules;
+    uint256 private vestingSchedulesTotalAmount;
+
+    event Released(uint256 amount);
+
+    /**
+    * @notice Returns the vesting schedule for specified address.
+    * @return the vesting schedule
+    */
+    function getVestingByAddress(address beneficiary) external view returns(VestingSchedule memory){
+        VestingSchedule memory vestingSchedule = vestingSchedules[beneficiary];
+        require(vestingSchedule.initialized, "TokenVesting: no vesting schedule for this address");
+        return vestingSchedule;
+    }
+
+    /**
+    * @notice Returns the total amount of vesting schedules.
+    * @return the total amount of vesting schedules
+    */
+    function getVestingSchedulesTotalAmount() external view returns(uint256){
+        return vestingSchedulesTotalAmount;
+    }
+    
+    /**
+    * @notice Release vested amount of tokens.
+    */
+    function release() public {
+        address beneficiary = msg.sender;
+        VestingSchedule storage vestingSchedule = vestingSchedules[beneficiary];
+        require(vestingSchedule.initialized, "TokenVesting: no vesting schedule for current sender");
+        uint256 vestedAmount = _computeReleasableAmount(vestingSchedule);
+        require(vestedAmount > 0, "TokenVesting: cannot release tokens, no vested tokens");
+        vestingSchedule.released = vestingSchedule.released.add(vestedAmount);
+        vestingSchedulesTotalAmount = vestingSchedulesTotalAmount.sub(vestedAmount);
+        _transfer(address(this), beneficiary, vestedAmount);
+    }
+
+    /**
+    * @notice Computes the vested amount of tokens for the given address.
+    * @return the vested token amount
+    */
+    function computeReleasableAmount(address beneficiary) public view returns(uint256){
+        VestingSchedule storage vestingSchedule = vestingSchedules[beneficiary];
+        require(vestingSchedule.initialized, "TokenVesting: no vesting schedule for this address");
+        require(vestingSchedule.start < block.timestamp, "TokenVesting: this vetsing has not started yet");
+        return _computeReleasableAmount(vestingSchedule);
+    }
+
+    /**
+    * @notice Creates a new vesting schedule for a beneficiary.
+    * @param beneficiary address of the beneficiary to whom vested tokens are transferred
+    * @param start start time of the vesting period
+    * @param duration duration in seconds of the period in which the tokens will vest
+    * @param period duration of a slice period for the vesting in seconds
+    * @param amount total amount of tokens to be released at the end of the vesting
+    */
+    function _createVestingSchedule(
+        address beneficiary,
+        uint256 start,
+        uint256 duration,
+        uint256 period,
+        uint256 amount
+    ) internal {
+        require(balanceOf(address(this)) >= amount,
+            "TokenVesting: cannot create vesting schedule because not sufficient tokens"
+        );
+        require(duration > 0, "TokenVesting: duration must be > 0");
+        require(amount > 0, "TokenVesting: amount must be > 0");
+        require(period >= 1, "TokenVesting: slicePeriodSeconds must be >= 1");
+        VestingSchedule storage vestingSchedule = vestingSchedules[beneficiary];
+        require(!vestingSchedule.initialized, "TokenVesting: the address is already have vesting shedule");
+        vestingSchedules[beneficiary] = VestingSchedule(
+            true,
+            start,
+            duration,
+            period,
+            amount,
+            0
+        );
+        vestingSchedulesTotalAmount = vestingSchedulesTotalAmount.add(amount);
+    }
+
+    /**
+    * @notice Computes the releasable amount of tokens for a vesting schedule.
+    * @return the amount of releasable tokens
+    */
+    function _computeReleasableAmount(VestingSchedule storage vestingSchedule) internal view returns(uint256){
+        uint256 currentTime = block.timestamp;
+        if (currentTime >= vestingSchedule.start.add(vestingSchedule.duration)) {
+            return vestingSchedule.amountTotal.sub(vestingSchedule.released);
+        } else {
+            uint256 timeFromStart = currentTime.sub(vestingSchedule.start);
+            uint256 period = vestingSchedule.period;
+            uint256 vestedPeriods = timeFromStart.div(period);
+            uint256 vestedSeconds = vestedPeriods.mul(period);
+            uint256 vestedAmount = vestingSchedule.amountTotal.mul(vestedSeconds).div(vestingSchedule.duration);
+            vestedAmount = vestedAmount.sub(vestingSchedule.released);
+            return vestedAmount;
+        }
+    }
+}
---
+++ contracts/TaleToken.sol
@@ -0,0 +1,232 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+import "./ERC20Vesting.sol";
+
+contract TaleToken is ERC20Vesting {
+
+    struct InstantBeneficiary {
+        address beneficiary;
+        uint256 amount;
+    }
+
+    uint256 private vestingPeriod = 2592000;                    // 30 days
+
+    uint256 private privateSaleVestingStart = 1645574400;       // 2022-02-23T00:00:00Z
+    uint256 private privateSaleVestingDuration = 31536000;      // 365 days
+    
+    uint256 private teamAndAdvisorVestingStart = 1677110400;    // 2023-02-23T00:00:00Z  
+    uint256 private teamAndAdvisorVestingDuration = 104976000;  // 1215 days
+    
+    uint256 private ecosystemVestingStart = 1647993600;         // 2022-03-23T00:00:00Z                
+    uint256 private ecosystemVestingDuration = 126230400;       // 4 years
+    
+    uint256 private marketingVestingStart = 1645574400;         // 2022-02-23T00:00:00Z
+    uint256 private marketingVestingDuration = 52272000;        // 605 days
+    
+    uint256 private exchangeFeeVestingStart = 1645574400;       // 2022-02-23T00:00:00Z
+    uint256 private exchangeFeeVestingDuration = 31536000;      // 365 days
+    
+    address[] private privateSaleVestingAddresses;
+    address[] private teamAndAdvisorVestingAddresses;
+    address[] private ecosystemVestingAddresses;
+    address[] private marketingVestingAddresses;
+    address[] private exchangeFeeVestingAddresses;
+
+    InstantBeneficiary public publicSaleBeneficiary;
+    InstantBeneficiary public privateSaleBeneficiary;
+    InstantBeneficiary public ecosystemBeneficiary;
+    InstantBeneficiary public exchangeFeeBeneficiary;
+
+    constructor(address publicSaleAddress, uint256 publicSaleAmount,
+        address privateSaleAdddress, uint256 privateSaleAmount,
+        address ecosystemAddress, uint256 ecosystemAmount,
+        address exchangeFeeAddress, uint256 exchangeFeeAmount) ERC20("Tales of Chain", "TALE") {
+            _mint(address(this), 2000000000 * 10 ** decimals());
+
+            _transfer(address(this), publicSaleAddress, publicSaleAmount);
+            _transfer(address(this), privateSaleAdddress, privateSaleAmount);
+            _transfer(address(this), ecosystemAddress, ecosystemAmount);
+            _transfer(address(this), exchangeFeeAddress, exchangeFeeAmount);
+            
+            publicSaleBeneficiary = InstantBeneficiary(publicSaleAddress, publicSaleAmount);
+            privateSaleBeneficiary = InstantBeneficiary(privateSaleAdddress, privateSaleAmount);
+            ecosystemBeneficiary = InstantBeneficiary(ecosystemAddress, ecosystemAmount);
+            exchangeFeeBeneficiary = InstantBeneficiary(exchangeFeeAddress, exchangeFeeAmount);
+    }
+
+    /**
+    * @notice Adds beneficiary and amount for private sale vesting
+    *         One address can participate in one vesting
+    *
+    * @param beneficiary address of the beneficiary to whom vested tokens are transferred
+    * @param amount total amount of tokens to be released at the end of the vesting
+    */
+    function addPrivateSaleVestingAddress(address beneficiary, uint256 amount) onlyOwner external {
+        _createVestingSchedule(
+            beneficiary, 
+            privateSaleVestingStart, 
+            privateSaleVestingDuration, 
+            vestingPeriod,
+            amount);
+        privateSaleVestingAddresses.push(beneficiary);
+    }
+
+    /**
+    * @notice Adds beneficiary and amount for team/advisor vesting
+    *         One address can participate in one vesting
+    *
+    * @param beneficiary address of the beneficiary to whom vested tokens are transferred
+    * @param amount total amount of tokens to be released at the end of the vesting
+    */
+    function addTeamAndAdvisorVestingAddress(address beneficiary, uint256 amount) onlyOwner external {
+        _createVestingSchedule(
+            beneficiary, 
+            teamAndAdvisorVestingStart, 
+            teamAndAdvisorVestingDuration, 
+            vestingPeriod,
+            amount);
+        privateSaleVestingAddresses.push(beneficiary);
+    }
+
+    /**
+    * @notice Adds beneficiary and amount for ecosustem vesting
+    *         One address can participate in one vesting
+    *
+    * @param beneficiary address of the beneficiary to whom vested tokens are transferred
+    * @param amount total amount of tokens to be released at the end of the vesting
+    */
+    function addEcosystemVestingAddress(address beneficiary, uint256 amount) onlyOwner external {
+        _createVestingSchedule(
+            beneficiary, 
+            ecosystemVestingStart, 
+            ecosystemVestingDuration, 
+            vestingPeriod,
+            amount);
+        ecosystemVestingAddresses.push(beneficiary);
+    }
+
+    /**
+    * @notice Adds beneficiary and amount for marketing vesting
+    *         One address can participate in one vesting
+    *
+    * @param beneficiary address of the beneficiary to whom vested tokens are transferred
+    * @param amount total amount of tokens to be released at the end of the vesting
+    */
+    function addMarketingVestingAddress(address beneficiary, uint256 amount) onlyOwner external {
+        _createVestingSchedule(
+            beneficiary, 
+            marketingVestingStart, 
+            marketingVestingDuration, 
+            vestingPeriod,
+            amount);
+        marketingVestingAddresses.push(beneficiary);
+    }
+
+    /**
+    * @notice Adds beneficiary and amount for exchange fee vesting
+    *         One address can participate in one vesting
+    *
+    * @param beneficiary address of the beneficiary to whom vested tokens are transferred
+    * @param amount total amount of tokens to be released at the end of the vesting
+    */
+    function addExchangeFeeVestingAddress(address beneficiary, uint256 amount) onlyOwner external {
+        _createVestingSchedule(
+            beneficiary, 
+            exchangeFeeVestingStart, 
+            exchangeFeeVestingDuration, 
+            vestingPeriod,
+            amount);
+        exchangeFeeVestingAddresses.push(beneficiary);
+    }
+
+    /**
+    * @notice Returns the number of benefeciary for private sale vesting
+    * @return the number of benefeciary
+    */
+    function getPrivateSaleVestingAddressCount() external view returns(uint256) {
+        return privateSaleVestingAddresses.length;
+    }
+
+    /**
+    * @notice Returns the number of benefeciary for team/advisor vesting
+    * @return the number of benefeciary
+    */
+    function getTeamAndAdvisorVestingAddressCount() external view returns(uint256) {
+        return teamAndAdvisorVestingAddresses.length;
+    }
+
+    /**
+    * @notice Returns the number of benefeciary for ecosystem vesting
+    * @return the number of benefeciary
+    */
+    function getEcosystemVestingAddressCount() external view returns(uint256) {
+        return ecosystemVestingAddresses.length;
+    }
+
+    /**
+    * @notice Returns the number of benefeciary for marketing vesting
+    * @return the number of benefeciary
+    */
+    function getMarketingVestingAddressCount() external view returns(uint256) {
+        return marketingVestingAddresses.length;
+    }
+
+    /**
+    * @notice Returns the number of benefeciary for exchange fee vesting
+    * @return the number of benefeciary
+    */
+    function getExchangeFeeVestingAddressCount() external view returns(uint256) {
+        return exchangeFeeVestingAddresses.length;
+    }
+
+    /**
+    * @notice Returns the benefeciary by index for private sale vesting
+    * @param index beneficiary index for private sale vesting
+    * @return the address of benefeciary
+    */
+    function getPrivateSaleVestingAddress(uint256 index) external view returns(address) {
+        require(index < privateSaleVestingAddresses.length);
+        return privateSaleVestingAddresses[index];
+    }
+
+    /**
+    * @notice Returns the benefeciary by index for team/advisor vesting
+    * @param index beneficiary index for team/advisor vesting
+    * @return the address of benefeciary
+    */
+    function getTeamAndAdvisorVestingAddress(uint256 index) external view returns(address) {
+        require(index < teamAndAdvisorVestingAddresses.length);
+        return teamAndAdvisorVestingAddresses[index];
+    }
+
+    /**
+    * @notice Returns the benefeciary by index for ecosystem vesting
+    * @param index beneficiary index for ecosystem vesting
+    * @return the address of benefeciary
+    */
+    function getEcosystemVestingAddress(uint256 index) external view returns(address) {
+        require(index < ecosystemVestingAddresses.length);
+        return ecosystemVestingAddresses[index];
+    }
+
+    /**
+    * @notice Returns the benefeciary by index for marketing vesting
+    * @param index beneficiary index for ecosystem vesting
+    * @return the address of benefeciary
+    */
+    function getMarketingVestingAddress(uint256 index) external view returns(address) {
+        require(index < marketingVestingAddresses.length);
+        return marketingVestingAddresses[index];
+    }
+
+    /**
+    * @notice Returns the benefeciary by index for exchange fee vesting
+    * @param index beneficiary index for exchange fee vesting
+    * @return the address of benefeciary
+    */
+    function getExchangeFeeVestingAddress(uint256 index) external view returns(address) {
+        require(index < exchangeFeeVestingAddresses.length);
+        return exchangeFeeVestingAddresses[index];
+    }
+}
